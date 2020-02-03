1e070bccc4001ad97e75bba6d9d4e78f0883933c
supriyaphalle <supriyaphalle@gmail.com>
2020-02-03 16:24:23 +0530

    MODIFIED: Created Function and Modified Total Amount

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -5,29 +5,43 @@ echo "Welcome to the GamblingSimulator"
 #veriables
 stake=100;
 total_amount=0
-
+win=0;
+loose=0;
+prev_stake=0;
 #Constants
 BET=1;
-MAX_VALUE=$(($stake+50*$stake / 100))
-MIN_VALUE=$(($stake-50*$stake/100))
+
+function calculativeGambler() {
+	MAX_VALUE=$(($stake+50*$stake / 100))
+	MIN_VALUE=$(($stake-50*$stake/100))
+	prev_stake=$stake
+	amount_per_day=0
+	while (( (($stake <= $MAX_VALUE)) && (($stake >= $MIN_VALUE)) ))
+	do
+		if (( $((RANDOM%2))==$BET))
+		then
+			stake=$(($stake+$BET))
+			win=$(($win+1))
+		else
+			stake=$(($stake-$BET))
+			loose=$(($loose+1))
+		fi
+	done
+	amount_per_day=$(($stake-$prev_stake))
+	echo $amount_per_day
+}
 
 
 for(( i=0;i<20;i++))
 do
-	while (( (($stake < $MAX_VALUE)) && (($stake > $MIN_VALUE)) ))
-	do
-		if (( $((RANDOM%2))==$BET))
-		then
-			stake=$(($stake+1))
-		else
-			stake=$(($stake-1))
-		fi
-	done
-	total_amount=$(($total_amount+$stake))
+	calculativeGambler
+	total_amount=$(($total_amount + $amount_per_day))
+
 done
 
-	echo "After 20 days of play total amount = $total_amount"
-
-
-
-
+if (($total_amount >0))
+then
+	echo "After 20 days of play, total amount win  = $total_amount"
+else
+	echo "After 20 days of play, total amount loss  =$(($total_amount * -1))"
+fi
