2776421bace69ae0113e81414c93c4e6051bc385
supriyaphalle <supriyaphalle@gmail.com>
2020-02-03 10:43:17 +0530

    ADDED:Minimum And Maximum Stake Conditions For A Day

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,16 +1,26 @@
-#! /bin/bash -x
+#! /bin/bash 
 
 echo "Welcome to the GamblingSimulator"
 
 stake=100;
 bet=1;
 
-if (( $((RANDOM%2))==$bet))
-then
-	echo "win the bet"
-else
-	echo "loose the bet"
-fi
+MAX_VALUE=$(($stake+50*$stake / 100))
+MIN_VALUE=$(($stake-50*$stake/100))
+
+while (( (($stake < $MAX_VALUE)) && (($stake > $MIN_VALUE)) ))
+do
+	if (( $((RANDOM%2))==$bet))
+	then
+#		echo "win the bet"
+		stake=$(($stake+1))
+	else
+#		echo "loose the bet"
+		stake=$(($stake-1))
+	fi
+
+done
+	echo "resign the game with stake of $stake"
 
 
 
