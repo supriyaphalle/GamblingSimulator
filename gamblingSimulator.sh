7c4fa0ad99b759c2c68bbd6c315a5d46c0a0a1c0
supriyaphalle <supriyaphalle@gmail.com>
2020-02-03 11:05:15 +0530

    ADDED: For Loop For 20 Days Of Play

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -2,25 +2,31 @@
 
 echo "Welcome to the GamblingSimulator"
 
+#veriables
 stake=100;
-bet=1;
+total_amount=0
 
+#Constants
+BET=1;
 MAX_VALUE=$(($stake+50*$stake / 100))
 MIN_VALUE=$(($stake-50*$stake/100))
 
-while (( (($stake < $MAX_VALUE)) && (($stake > $MIN_VALUE)) ))
-do
-	if (( $((RANDOM%2))==$bet))
-	then
-#		echo "win the bet"
-		stake=$(($stake+1))
-	else
-#		echo "loose the bet"
-		stake=$(($stake-1))
-	fi
 
+for(( i=0;i<20;i++))
+do
+	while (( (($stake < $MAX_VALUE)) && (($stake > $MIN_VALUE)) ))
+	do
+		if (( $((RANDOM%2))==$BET))
+		then
+			stake=$(($stake+1))
+		else
+			stake=$(($stake-1))
+		fi
+	done
+	total_amount=$(($total_amount+$stake))
 done
-	echo "resign the game with stake of $stake"
+
+	echo "After 20 days of play total amount = $total_amount"
 
 
 
