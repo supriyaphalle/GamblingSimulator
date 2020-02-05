8da89b735c828584fba38e358205bbeafeaf779e
supriyaphalle <supriyaphalle@gmail.com>
2020-02-05 12:47:36 +0530

    ADDED:Sort Function To Find Luciest and Unluciest Day

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,13 +1,12 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 echo "Welcome to the GamblingSimulator"
 
 declare -A  amount
+declare -A  winloss
 
 #veriables
 total_amount=0
-win=0;
-loose=0;
 
 #Constants
 stake=100
@@ -25,39 +24,47 @@ function calculativeGambler() {
 		if (( $((RANDOM%2))==$BET))
 		then
 			stake=$(($stake+$BET))
-			win=$(($win+1))
 		else
 			stake=$(($stake-$BET))
-			loose=$(($loose+1))
 		fi
 	done
 	amount_per_day=$(($stake-$prev_stake))
 }
 
 function gamblerForMonth() {
-for(( i=1;i<20;i++))
+for(( i=1;i<=20;i++))
 do
    calculativeGambler
 	amount[day$i]=$amount_per_day
 	total_amount=$(($total_amount+$amount_per_day))
-	if (( ${amount[day$i]}>0 ))
-	then
-		echo "win of $i day = ${amount[day$i]}"
-	else
-		echo "loss of $i day = $((${amount[day$i]} ))"
-	fi
+	echo "Total Amount= $total_amount"
+	winloss[day$i]=$total_amount
 done
 }
 
-
-
+function displayTotalAmount() {
+	if (($total_amount >0))
+	then
+		echo "After 20 days of play, total amount win  = $total_amount"
+	else
+		echo "After 20 days of play, total amount loss  =$(($total_amount * -1))"
+	fi
+}
 
 #################################### Start Program##################################
 gamblerForMonth
-if (($total_amount >0))
-then
-	echo "After 20 days of play, total amount win  = $total_amount"
-else
-	echo "After 20 days of play, total amount loss  =$(($total_amount * -1))"
-fi
-#echo ${amount[@]}
+displayTotalAmount
+
+printf "Luciest Day : "
+for i in ${!winloss[@]}
+do
+	echo " $i  ${winloss[$i]}"
+done | sort -k2 -rn | head -1
+
+
+printf "Unluciest Day : "
+for i in ${!winloss[@]}
+do
+	echo  "$i ${winloss[$i]}"
+done | sort -k2 -rn | tail -1
+
